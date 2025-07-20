c80cb157c7650ebd75bef9fa732c8a6754d50e2f
C4Zamora <caroldzamora4@gmail.com>
2025-07-20 01:14:25 -0500

    commit

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,30 +1,28 @@
-import { Dashboard } from "../pages/dashboard/dashboard"; 
+import { createBrowserRouter } from "react-router-dom";
+import { Dashboard } from "../pages/dashboard/dashboard";
 import { Login } from "../pages/login/login";
 import { Registro } from "../pages/register/register";
-import { createBrowserRouter } from "react-router-dom";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    Component: () => {
-      window.location.replace("/jd/login");
-      return null;
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      Component: Login, // O redirige correctamente con Navigate
     },
-  },
+    {
+      path: "/login",
+      Component: Login,
+    },
+    {
+      path: "/registro",
+      Component: Registro,
+    },
+    {
+      path: "/dashboard",
+      Component: Dashboard,
+    },
+  ],
   {
-    path: "/jd/login",
-    Component: Login,
-  },
-  {
-    path: "jd/registro",
-    Component: Registro,
-  },
-  {
-    path: "jd/dashboard",
-    Component: Dashboard,
-  },
-   {
-    basename: "/jd/login", // Cambia esto según el nombre de tu repositorio
+    basename: "/jd", // 🔥 Aquí es donde se indica que todo está bajo /jd
   }
-]);
-
+);
